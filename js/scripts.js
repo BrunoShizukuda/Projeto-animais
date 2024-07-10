22fb3dad3edf36c2bc02d456ae6c49bb5ec75e08
Bruno Shizukuda <shizukudabruno@gmail.com>
2024-07-10 14:36:47 -0300

    att

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+import initAnimacaoScroll from './module/scroll-animacao.js'
+import initScrollSuave from "./module/scroll-suave.js"
+import initAccordion from "./module/accordion.js"
+import initTabNav from "./module/init-tab.js"
+import initModal from "./module/modal.js"
+import initTooltip from "./module/tooltip.js"
+import initDropDownMenu from "./module/dropdown-menu.js"
+import initMenuMobile from './module/menumobile.js'
+import initAnimaNumeros from './module/anima-numeros.js'
+
+
+
+
+initDropDownMenu()
+initModal()
+initTabNav()
+initAccordion()
+initScrollSuave()
+initAnimacaoScroll()
+initTooltip()
+initMenuMobile()
+initAnimaNumeros()
